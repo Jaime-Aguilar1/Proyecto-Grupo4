021d390da53b5320089537dc866efe9770ccbdfe
fabda21 <fadamuor@gmail.com>
2026-03-06 20:59:34 -0600

    Model Reserva

---
+++ Models/Reserva.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Google.Cloud.Firestore;
+using static FirebaseAdmin.FirebaseApp;
+
+namespace Proyecto.API.Models
+{
+
+    [FirestoreData]
+    public class Reserva
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+
+        [FirestoreProperty]
+        public string IdUsuario { get; set; }
+
+        [FirestoreProperty]
+        public string NombreUsuario { get; set; }
+
+        [FirestoreProperty]
+        public string IdHabitacion { get; set; }
+
+        [FirestoreProperty]
+        public string NumeroHabitacion { get; set; }
+
+        [FirestoreProperty]
+        public string TipoHabitacion { get; set; }
+
+        [FirestoreProperty]
+        public DateTime FechaCheckIn { get; set; }
+
+        [FirestoreProperty]
+        public DateTime FechaCheckOut { get; set; }
+
+        [FirestoreProperty]
+        public int Noches { get; set; }
+
+        [FirestoreProperty]
+        public decimal CostoTotal { get; set; }
+
+        [FirestoreProperty]
+        public DateTime Timestamp { get; set; }
+    }
+}
