e77a47276329f0bc1f2dc80eb841fac76cad7ca5
Axl-Ramses <ramses.coto1@gmail.com>
2026-03-13 18:20:53 -0600

    Implementacion de AuthService, IAuthService y arreglo de UserDto

---
+++ Services/IAuthService.cs
@@ -0,0 +1,40 @@
+﻿using Proyecto_Grupo4.API.DTOs;
+using Proyecto.API.Models;
+namespace Proyecto_Grupo4.API.Services;
+
+
+public interface IAuthService
+{
+    /**
+     * Register:
+     * 1. Verificar que el correo no esté duplicado en Firestore
+     * 2. Crear el objeto Usuario con los datos del hotel
+     * 3. Guardar el documento en la colección "Usuarios"
+     * * Retorna: El usuario creado (modelo Usuario)
+     */
+    Task<Usuario> Register(RegisterDto registerDto);
+
+    /**
+     * Login:
+     * 1. Validar que el email existe en Firestore
+     * 2. Verificar la contraseña (campo 'Costrasena')
+     * 3. Si son correctas, generar el JWT
+     * * Retorna: Una tupla con los datos del usuario mapeados al DTO y el token generado
+     */
+    Task<(UserDto user, string token)> Login(LoginDto loginDto);
+
+    /**
+     * Obtiene los datos de un usuario específico por su ID de documento
+     */
+    Task<Usuario?> GetUserById(string userId);
+
+    /**
+     * Valida si un token JWT recibido es legítimo
+     */
+    Task<bool> ValidateToken(string token);
+
+    /**
+     * Genera la cadena del token JWT basada en los datos del usuario y su rol (huésped/gerente)
+     */
+    string GenerateJwtToken(Usuario user);
+}
